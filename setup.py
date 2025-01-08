b233b89f4c9370a78f910f0c8423cc341b880f1c
Siddharth Garimella <gsidsid@gmail.com>
2025-01-07 22:21:34 -0800

    version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ancilla",
-    version="0.1.0",
+    version="1.1.0",
     packages=find_packages(),
     install_requires=[
         "polygon-api-client>=1.12.0",
